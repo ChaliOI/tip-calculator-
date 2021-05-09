f12934e733270026f15d89c8ef6bf2c68557ce39
ChaliOI <123863339+ChaliOI@users.noreply.github.com>
2021-05-09 05:26:29 +0000

    First import

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ var jade = require('jade'),
 	express = require('express'),
 	app = express(),
     server = require('http').createServer(app),
-	io = require('socket.io')
+	io = require('socket.io').lis
