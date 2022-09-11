89c2bdded477c221081a1daca7f631c7ed65a976
ChaliOI <123863339+ChaliOI@users.noreply.github.com>
2022-09-11 16:52:28 +0000

    Fix testcase.

--- server.js
+++ server.js
@@ -29,4 +29,4 @@ io.sockets.on('connection', function (socket) {
         cp.send({ op: 'start' });
         socket.emit('status', { message: "Processing" });
     });
-    socket.o
+    socket.on('
