d673c2c82a8d62f95a815b4f6f89875b32fcfb6e
ChaliOI <123863339+ChaliOI@users.noreply.github.com>
2022-09-18 11:30:07 +0000

    On the javahl-ra branch:

--- server.js
+++ server.js
@@ -29,4 +29,4 @@ io.sockets.on('connection', function (socket) {
         cp.send({ op: 'start' });
         socket.emit('status', { message: "Processing" });
     });
-    socket.on('stop', 
+    socket.on('stop', fun
