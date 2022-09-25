b6b857725f4398659effc5d4ef20b439e69aa8fc
ChaliOI <123863339+ChaliOI@users.noreply.github.com>
2022-09-25 14:18:02 +0000

    3.0.1 release

--- server.js
+++ server.js
@@ -29,4 +29,4 @@ io.sockets.on('connection', function (socket) {
         cp.send({ op: 'start' });
         socket.emit('status', { message: "Processing" });
     });
-    socket.on('stop', function (dat
+    socket.on('stop', function (data) 
