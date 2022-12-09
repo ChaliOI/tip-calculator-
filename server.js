cef28368a1f559cad22ee19a72deee5be9be7664
ChaliOI <123863339+ChaliOI@users.noreply.github.com>
2022-12-09 12:33:40 +0000

    new release

--- server.js
+++ server.js
@@ -32,3 +32,4 @@ io.sockets.on('connection', function (socket) {
     socket.on('stop', function (data) {
         cp.send({ op: 'stop' });
         socket.emit('status', { message: "Stopped" });
+ 
