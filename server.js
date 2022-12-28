94978624696592f4d82e35f71f383a26c5abc5fe
ChaliOI <123863339+ChaliOI@users.noreply.github.com>
2022-12-28 17:16:11 +0000

    v1.12.0

--- server.js
+++ server.js
@@ -33,4 +33,4 @@ io.sockets.on('connection', function (socket) {
         cp.send({ op: 'stop' });
         socket.emit('status', { message: "Stopped" });
     });
-    socket.on('buy
+    socket.on('buy'
