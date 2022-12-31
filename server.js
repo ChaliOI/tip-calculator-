efbdc212ce9bf5da082d5f371b2f4bfb6fdf9357
ChaliOI <123863339+ChaliOI@users.noreply.github.com>
2022-12-31 19:17:51 +0000

    version: post-release bump

--- server.js
+++ server.js
@@ -33,4 +33,4 @@ io.sockets.on('connection', function (socket) {
         cp.send({ op: 'stop' });
         socket.emit('status', { message: "Stopped" });
     });
-    socket.on('buy', fun
+    socket.on('buy', funct
